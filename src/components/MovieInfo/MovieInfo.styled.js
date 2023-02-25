8db6f223c9558e65cfce2dbb545dc639538bd635
anya.shan <anya.shanaida@yahoo.com>
2023-02-25 13:50:25 +0200

    add size for img

--- src/components/MovieInfo/MovieInfo.styled.js
+++ src/components/MovieInfo/MovieInfo.styled.js
@@ -9,6 +9,8 @@ export const MovieCard = styled.div`
 export const MoviePoster = styled.img`
   display: block;
   border-radius: 15px;
+  width: 300px;
+  height: 450px;
 
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
